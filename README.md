0c7897aea041229fceb966892fb2b1b1c6d8e79f
Xinyi Liao <xlia4305@uni.sydeny.edu.au>
2023-05-25 23:55:16 +1000

    style the input form and with more details

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # xlia4305-tracker
+## References
+habibmhamadi. (2022). multi-select-tag. Retrieved 12 May 2023, from https://github.com/habibmhamadi/multi-select-tag
--- public/script.js
+++ public/script.js
@@ -1,7 +1,10 @@
+// change the display mode of the input form so it appears
 function openForm () {
     document.getElementById('input').style.display = 'block';
+    document.body.style.backgroundColor = 'rgb(0,0,0,0.5)';//add transparent background
 }
-
+//hide the input form and background color
 function closeForm() {
     document.getElementById('input').style.display = 'none';
+    document.body.style.backgroundColor = 'rgb(0,0,0,0)';
 }
