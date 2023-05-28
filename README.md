219bd31ea9fe7476225801f8e522ed9f3757b14e
Xinyi Liao <xlia4305@uni.sydeny.edu.au>
2023-05-28 22:59:46 +1000

    Adding localStorage features in js and an overview section in html

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-# xlia4305-tracker
+# Development Documentation
+## Home page
+## User input form
+![Desktop Home page comparison]
+()
+Mockup(Up) and Prototype(down)
+![Mobile Home page comparison]
+()
+Mockup(left) and Prototype(right)
+
+### Development process
+### Iterations
+### Improvements
+### Lessons learned & Best practices
+## Overview
+### Lessons learned & Best practices
+* I attempted to utilize the IMDb API to locate the exact movie poster for the thumbnail image. However, it appears that all the available APIs require payment for permission to use. Thus, I chose to use preset images that align with the film's genre instead.
+## Application Configuration
+### Screen dimensions and resolutions best for viewing the application
+**Dimensions** (width x depth): 13-inch Macbook (30.41 x 21.24 cm)
+**Resolutions**: 2560 x 1600 pixels
+### Steps required to interact with the application
+1.
+2.
+3.
+
 ## Deployment Procedures:
-Download vanilla tilt effects with
+Download Glide.js for carousel effects with
 ```
-npm install vanilla-tilt
+npm install @glidejs/glide
 ```
 
 To start the server:
@@ -13,5 +38,5 @@ npm run start
 habibmhamadi. (2022). *multi-select-tag* [JS Library]. 
 https://github.com/habibmhamadi/multi-select-tag
 
-micku7zu. (2022). Vanilla-tilt.js [JS Library]. 
-https://github.com/micku7zu/vanilla-tilt.js 
+jedrzejchalubek. (2022). glide.js [JS Library]. 
+https://github.com/glidejs/glide
--- public/script.js
+++ public/script.js
@@ -1,10 +1,121 @@
+//Handling images using glob resolvers, refer to the code from https://github.com/robdongas/deco2017-task-tracker/commit/b070dc4ff3d621b124326d04366782299a4961c8?diff=split
+import images from './images/thumbnails/*.png';
+ console.log(images)
+
+//Create a function to add the new film entry to the array
+function addFilm(name, directors, characters, genres, rating, review) {
+    // Create the object to store the parameters and informations of a film entry
+    let film = {
+        name,
+        directors,
+        characters,
+        genres,
+        rating,
+        review,
+        id: Date.now(),//automatically generating an unique identifier for the new entry
+        date: new Date().toISOString(),//automatically record the date when the entry is added
+    }
+
+    //fetching and parse a string from localStorage into js structure
+    let localFilms = JSON.parse(localStorage.getItem('films'));
+    //if there's nothing in the localStorage, overwrite it with a list containig the current film entry
+    if (localFilms == null) {
+        localFilms = [film];
+    } else {
+        //Check if there's already a film with the same ID exited
+        if (localFilms.find(element => element.id === film.id)) {
+            console.log('Task ID already exists');//log an error message
+        } else {
+            localFilms.push(film);//otherwise add the new entry to localStorage
+        }
+    }
+    //Turn the array into a string to store in localStorage
+    localStorage.setItem('films', JSON.stringify(localFilms));
+
+    displayFilms();
+}
+
+//A function that loops through localStorage to display all the stored film entries
+function displayFilms() {
+    //clear the HTML list first
+    filmlist.innerHTML = '';
+    //Fetch and parse film entries array from localStorage
+    let localFilms = JSON.parse(localStorage.getItem('films'));
+    //Make sure localStorage is not empty
+    if (localFilms !== null) {
+        //align image to the genre of the film item in the array
+        localFilms.forEach(function(film) {
+            let filmImage = null;
+            switch (film.genres.value) {
+
+            }
+            //Create items for the DOM and add to the display list
+            let item = document.createElement('li');
+            item.setAttribute('data-id', film.id);
+            item.innerHTML = `<h2>${film.genres}</h2><h2>${film.rating}</h2><img src='${taskImage}' width='50'/><h1>${film.name}</h1><h2>Directed by: <strong>${film.directors}</strong></h2>
+            <h2>Fave Character(s): <strong>${film.characters}</strong></h2><p class='review'>${film.review}</p><p class='date'>${film.date}</p>`;
+            filmlist.appendChild(item);
+
+            //Clear the value of the input once the task has been added to the page
+            form.reset();
+
+            //Setup delete button DOM elements
+            let delButton = document.createElement('button');
+            let delButtonText = document.createTextNode('Delete');
+            delButton.appendChild(delButtonText);
+            item.appendChild(delButton); //Adds a delete button to every film entry
+
+            //Listen for when the delete button is clicked
+            delButton.addEventListener('click', function (event) {
+                localFilms.forEach(function (taskArrayElement, taskArrayIndex) {
+                    if (taskArrayElement.id == item.getAttribute('data-id')) {
+                        localFilms.splice(taskArrayIndex, 1)
+                    }
+                })
+                //Update localStorage with the newly spliced array
+                localStorage.setItem('tasks', JSON.stringify(localFilms));
+                //remove the film item from the page when delete button is clicked
+                item.remove();
+            })
+        })//Closing brackets for for loop
+    }//Closing bracket for if statement
+}
+
+//Create variables to hold HTML elements using DOM
+const form = document.getElementById('form-container');
+const filmlist = document.getElementsByClassName('glide__slides');
+
 // change the display mode of the input form so it appears
 function openForm () {
     document.getElementById('input').style.display = 'block';
     document.body.style.backgroundColor = 'rgb(0,0,0,0.5)';//add transparent background
 }
-//hide the input form and background color
-function closeForm() {
-    document.getElementById('input').style.display = 'none';
-    document.body.style.backgroundColor = 'rgb(0,0,0,0)';
+
+//When save button is clicked, hide the input form and store the new entry to the local storage
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); //stop the event from reloading the page and send out the information by default
+    document.getElementById('input').style.display = 'none';//hide the form
+    document.body.style.backgroundColor = 'rgb(0,0,0,0)';//get rid of the background color
+    //call the function to store the new entry 
+    addFilm(
+        form.elements.filmName.value,
+        form.elements.director.value,
+        form.elements.character.value,
+        form.elements.genre.value,
+        form.elements.rating.value,
+        form.elements.review.value,
+        )
+})
+
+//function that controls the overview section to slide up/down
+function slideUp(el) {
+    var elem = document.getElementById(el);
+    elem.style.transition = 'all 1s ease-in-out';
+    elem.style.transform = 'translateY(-1400px)';
+}
+
+function slideDown(el) {
+    var elem = document.getElementById(el);
+    elem.style.transition = 'all 1s ease-in-out';
+    elem.style.transform = 'translateY(1400px)';
 }
